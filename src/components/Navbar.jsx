0254adba635e5d398838460d2ff5323aa523c1a7
muthomi100 <techeasesolns@gmail.com>
2024-12-03 22:40:14 +0300

    Add some padding styles for responsiveness."

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,37 +4,39 @@ import DarkModeToggle from "./DarkModeToggle";
 const Navbar = () => {
   const linkClass = ({ isActive }) =>
     isActive
-      ? "bg-black text-white hover:bg-gray-800 rounded-lg px-4 py-2 "
-      : "text-white hover:bg-gray-800 rounded-lg px-4 py-2 ";
+      ? "bg-black text-white hover:bg-gray-800 rounded-sm px-1 py-1 "
+      : "text-white hover:bg-gray-800 rounded-sm px-1 py-1";
   return (
     <>
-      <nav
-        className="bg-indigo-700 border-b border-indigo-500 shadow-md"
-        aria-label="Main navigation"
-      >
-        <div className="mx-auto max-w-7xl ml-2 px-4 sm:px-6 lg:px-8">
-          <div className="flex h-16 items-center justify-between">
-            <div className="flex items-center space-x-4">
-              <NavLink to="/" className="text-white text-2xl font-extrabold">
-                Recipe Tracker
-              </NavLink>
-            </div>
+      <div className="text-base sm:text-lg md:text-xl lg:text-2xl">
+        <nav
+          className="bg-indigo-700 border-b border-indigo-500 shadow-md"
+          aria-label="Main navigation"
+        >
+          <div className="mx-auto max-w-7xl ml-2 px-4 sm:px-6 lg:px-8">
+            <div className="flex h-16 items-center justify-between">
+              <div className="flex items-center space-x-4">
+                <NavLink to="/" className="text-white text-2xl font-extrabold">
+                  Recipe Tracker
+                </NavLink>
+              </div>
 
-            <div className="flex space-x-4">
-              <NavLink to="/" className={linkClass}>
-                Home
-              </NavLink>
-              <NavLink to="/recipes" className={linkClass}>
-                Recipes
-              </NavLink>
-              <NavLink to="/add-recipe" className={linkClass}>
-                Add Recipe
-              </NavLink>
-              <DarkModeToggle />
+              <div className="flex space-x-4">
+                <NavLink to="/" className={linkClass}>
+                  Home
+                </NavLink>
+                <NavLink to="/recipes" className={linkClass}>
+                  Recipes
+                </NavLink>
+                <NavLink to="/add-recipe" className={linkClass}>
+                  Add Recipe
+                </NavLink>
+                <DarkModeToggle />
+              </div>
             </div>
           </div>
-        </div>
-      </nav>
+        </nav>
+      </div>
     </>
   );
 };
