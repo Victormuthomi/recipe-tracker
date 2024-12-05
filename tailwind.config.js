e8af5d6e28f0e13efe2bf8f70263dc76dc4c33e6
muthomi100 <techeasesolns@gmail.com>
2024-12-05 21:49:36 +0300

    Add a random recipe if no recipes already added on  the localStorage.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  darkMode: "class",
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Roboto", "sans-serif"],
-      },
-      gridTemplateColumns: {
-        "70/30": "70% 28%",
-      },
+export const content = ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"];
+export const darkMode = "class";
+export const theme = {
+  extend: {
+    fontFamily: {
+      sans: ["Roboto", "sans-serif"],
+    },
+    gridTemplateColumns: {
+      "70/30": "70% 28%",
     },
   },
-  plugins: [],
 };
+export const plugins = [];
